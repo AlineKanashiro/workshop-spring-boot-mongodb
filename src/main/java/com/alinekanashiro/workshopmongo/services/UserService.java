02c7c98867e1cf0fcb9c4dd75f933137abd874b6
Aline Kanashiro <aline.k.kanashiro@gmail.com>
2025-05-06 21:06:41 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/alinekanashiro/workshopmongo/services/UserService.java
@@ -0,0 +1,21 @@
+package com.alinekanashiro.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alinekanashiro.workshopmongo.domain.User;
+import com.alinekanashiro.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+	
+}
