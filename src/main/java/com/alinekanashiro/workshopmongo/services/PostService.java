eaacb8b7bcfd324b404dec1f9d879596974c8b95
Aline Kanashiro <aline.k.kanashiro@gmail.com>
2025-05-11 09:53:04 -0300

    Obtendo um post por id

---
+++ src/main/java/com/alinekanashiro/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.alinekanashiro.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alinekanashiro.workshopmongo.domain.Post;
+import com.alinekanashiro.workshopmongo.repository.PostRepository;
+import com.alinekanashiro.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
